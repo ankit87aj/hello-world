edaa57a196e3cbe0027d308c051adb9e0bd508c9
ankit87aj <ankit.work413@gmail.com>
2016-05-29 12:21:52 -0700

    Collapsing
    Toolbar

---
+++ ExampleUnitTest.java
@@ -0,0 +1,20 @@
+package com.ramation.foldingcell.examples.celstream;
+
+/**
+ * Created by Ankit on 28-05-2016.
+ */
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
--- MainActivity.java
+++ MainActivity.java
@@ -1,66 +1,106 @@
-package com.ramation.foldingcell.examples.myapplication;
+package com.ramation.foldingcell.examples.celstream;
 
-import android.graphics.Color;
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.graphics.Rect;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
-import com.ramotion.foldingcell.FoldingCell;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.recycler_view)
+    protected RecyclerView mRecyclerView;
+
+    @Bind(R.id.toolbar)
+    protected Toolbar mToolbar;
+    List<String> dataSet = null;
+    Handler handlingData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        ButterKnife.bind(this);
+        setSupportActionBar(mToolbar);
+        new Thread(new Runnable() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void run() {
+                try {
+                    dataSet = getImage(MainActivity.this);
+                    Message msgObj = handler.obtainMessage();
+                    Bundle b = new Bundle();
+                    b.putString("message", "load");
+                    msgObj.setData(b);
+                    handler.sendMessage(msgObj);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }).start();
+
+        mRecyclerView.addItemDecoration(new RecyclerView.ItemDecoration() {
+            @Override
+            public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+                super.getItemOffsets(outRect, view, parent, state);
+                outRect.bottom = getResources().getDimensionPixelSize(R.dimen.activity_vertical_margin);
             }
         });
-    // get our folding cell
-    final FoldingCell fc = (FoldingCell) findViewById(R.id.folding_cell);
-        // set custom parameters
-        fc.initialize(1000, Color.DKGRAY, 2);
-            // attach click listener to folding cell
-            fc.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            fc.toggle(false);
-        }
-    });
-}
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    private List<String> getImage(Context context) throws IOException
+    {
+        AssetManager assetManager = context.getAssets();
+        String[] files = assetManager.list("");
+        List<String> it= Arrays.asList(files);
+        return it;
     }
+    // Define the Handler that receives messages from the thread and update the progress
+    private final Handler handler = new Handler() {
+
+        public void handleMessage(Message msg) {
+
+            String aResponse = msg.getData().getString("message");
+
+            if ((null != aResponse)) {
+
+                if (aResponse.equalsIgnoreCase(aResponse)) {
+                    displayData();
+                } else {
+
+                }
+            } else {
+
+                // ALERT MESSAGE
+                Toast.makeText(
+                        getBaseContext(),
+                        "Not Got Response From Server.",
+                        Toast.LENGTH_SHORT).show();
+            }
+
+        }
+    };
+
+
+    public void displayData() {
+        PhotoAdapter adapter = new PhotoAdapter(dataSet, this);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerView.setAdapter(adapter);
+    }
+
 }
